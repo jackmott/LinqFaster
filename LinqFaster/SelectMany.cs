9d8f1a79cba4a2eb0ee892befcc2a961e8c1893d
jackmott <jack.mott@gmail.com>
2017-03-27 08:58:08 -0500

    selectmany

---
+++ LinqFaster/SelectMany.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFaster
+{
+    public static class SelectManyFuncs
+    {
+        public static TResult[] SelectMany<TSource, TResult>(this TSource[] source, Func<TSource, TResult[]> selector)
+        {
+            var result = new List<TResult>();            
+            for (int i = 0; i < source.Length; i++)
+            {
+                var va = selector(source[i]);
+                for (int j = 0; j < va.Length; j++)
+                {
+                    result.Add(va[j]);
+                }         
+            }
+            return result.ToArray();
+        }
+
+        public static List<TResult> SelectMany<TSource, TResult>(this List<TSource> source, Func<TSource, List<TResult>> selector)
+        {
+            var result = new List<TResult>();
+            for (int i = 0; i < source.Count; i++)
+            {
+                var va = selector(source[i]);
+                for (int j = 0; j < va.Count; j++)
+                {
+                    result.Add(va[j]);
+                }
+            }
+            return result;
+        }
+    }
+}
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqFaster;
+using System.Diagnostics;
 
 namespace Tests
 {
     class Program
     {
+       
         static void Main(string[] args)
-        {
+        {            
         }
     }
 }
