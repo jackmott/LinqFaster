13fe63be0e7251c266fb5f0dad94df8d5aa97d78
jackmott <jack.mott@gmail.com>
2017-03-27 13:29:06 -0500

    reverse

---
+++ LinqFaster/Reverse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFaster
+{
+    public static class ReverseFuncs
+    {
+        public static T[] Reverse<T>(this T[] source)
+        {
+            var result = new T[source.Length];
+            long lenLessOne = source.Length - 1;
+            for (long i = 0; i < result.LongLength;i++)
+            {
+                result[i] = source[lenLessOne - i];
+            }
+            return result;
+        }
+
+        public static List<T> Reverse<T>(this List<T> source)
+        {
+            var result = new List<T>(source.Count);                        
+            for (int i = result.Count-1; i >= 0; i--)
+            {
+                result.Add(source[i]);
+            }
+            return result;
+        }
+
+
+    }
+}
