9af226d7b2db4178d1fef3e9472be0b46b649ad6
jackmott <jack.mott@gmail.com>
2017-03-29 16:57:22 -0500

    tests

--- Tests/AggregateTests.cs
+++ Tests/AggregateTests.cs
@@ -12,6 +12,46 @@ namespace Tests
         [Test]
         public void AggregateArray()
         {
+            Func<int, int, int> lambda = (x, acc) => acc += x;
+            var a = Program.intArray.AggregateF(lambda);
+            var b = Program.intArray.Aggregate(lambda);
+            Assert.That(a, Is.EqualTo(b));
+
+            Func<string, int, string> lambda2 = (acc, x) => acc += x;
+
+            var seed = "seed";
+            var c = Program.intArray.AggregateF(seed,lambda2);
+            var d = Program.intArray.Aggregate(seed,lambda2);
+
+            Assert.That(a, Is.EqualTo(b));
+
+            var e = Program.intArray.AggregateF(seed, lambda2, (x => ""));
+            var f = Program.intArray.Aggregate(seed, lambda2, (x => ""));
+
+            Assert.That(e, Is.EqualTo(f));
+
+        }
+
+        [Test]
+        public void AggregateList()
+        {
+            Func<int, int, int> lambda = (x, acc) => acc += x;
+            var a = Program.intList.AggregateF(lambda);
+            var b = Program.intList.Aggregate(lambda);
+            Assert.That(a, Is.EqualTo(b));
+
+            Func<string, int, string> lambda2 = (acc, x) => acc += x;
+
+            var seed = "seed";
+            var c = Program.intList.AggregateF(seed, lambda2);
+            var d = Program.intList.Aggregate(seed, lambda2);
+
+            Assert.That(c, Is.EqualTo(d));
+
+            var e = Program.intList.AggregateF(seed, lambda2, (x => ""));
+            var f = Program.intList.Aggregate(seed, lambda2, (x => ""));
+
+            Assert.That(e, Is.EqualTo(f));
 
         }
     }
---
+++ Tests/AnyAllTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using JM.LinqFaster;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    class AnyAllTests
+    {
+        [Test]
+        public void AnyArray()
+        {
+            
+        }
+
+        [Test]
+        public void AllArray()
+        {
+            
+        }
+    }
+}
