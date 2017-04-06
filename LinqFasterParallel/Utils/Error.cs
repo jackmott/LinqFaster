4d93e986ea60e5c01d1fa9d983076105e7c55df0
jack mott <jack.mott@gmail.com>
2017-04-05 22:14:48 -0500

    more unity compat

--- LinqFasterParallel/Utils/Error.cs
+++ LinqFasterParallel/Utils/Error.cs
@@ -2,17 +2,17 @@
 
 internal static class Error
 {
-    internal static Exception ArgumentNull(string s) => new ArgumentNullException(s);
+    internal static Exception ArgumentNull(string s) { return new ArgumentNullException(s); }
 
-    internal static Exception ArgumentOutOfRange(string s) => new ArgumentOutOfRangeException(s);
+    internal static Exception ArgumentOutOfRange(string s) { return new ArgumentOutOfRangeException(s); }
 
-    internal static Exception MoreThanOneElement() => new InvalidOperationException("Sequence contains more than one element");
+    internal static Exception MoreThanOneElement() { return new InvalidOperationException("Sequence contains more than one element"); }
 
-    internal static Exception MoreThanOneMatch() => new InvalidOperationException("Sequence contains more than one matching element");
+    internal static Exception MoreThanOneMatch() { return new InvalidOperationException("Sequence contains more than one matching element"); }
 
-    internal static Exception NoElements() => new InvalidOperationException("Sequence contains no elements");
+    internal static Exception NoElements() { return new InvalidOperationException("Sequence contains no elements"); }
 
-    internal static Exception NoMatch() => new InvalidOperationException("Sequence contains no matching element");
+    internal static Exception NoMatch() { return new InvalidOperationException("Sequence contains no matching element"); }
 
-    internal static Exception NotSupported() => new NotSupportedException();
+    internal static Exception NotSupported() { return new NotSupportedException(); }
 }
